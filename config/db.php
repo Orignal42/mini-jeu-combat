8f3d98b5edc0b13af933a36df46fc9ef3a39350b
Orignal42 <rbarriquand@gmail.com>
2021-03-25 11:14:02 +0100

    1er depot

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+try
+{
+    $db = new PDO('mysql:host=127.0.0.1;dbname=Combat','root','');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
